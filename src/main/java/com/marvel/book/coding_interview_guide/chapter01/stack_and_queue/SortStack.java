efc61678afccbb120185b12de12763139da5071f
cug09evan <cug09evan@163.com>
2019-05-30 10:14:53 +0800

    DEV：用一个栈实现另外一个栈的排序实现完成

---
+++ src/main/java/com/marvel/book/coding_interview_guide/chapter01/stack_and_queue/SortStack.java
@@ -0,0 +1,59 @@
+package com.marvel.book.coding_interview_guide.chapter01.stack_and_queue;
+
+import com.alibaba.fastjson.JSON;
+
+import java.util.Stack;
+
+/**
+ * 用一个栈实现另外一个栈的排序
+ */
+public class SortStack {
+
+    private static void sortStackByStack(Stack<Integer> stack) {
+        Stack<Integer> tempStack = new Stack<>();
+        tempStack.push(stack.pop());
+        while (!stack.empty()) {
+            if (stack.peek() >= tempStack.peek()) {
+                tempStack.push(stack.pop());
+            } else {
+                int tempElem = stack.pop();
+                while (!tempStack.empty()) {
+                    stack.push(tempStack.pop());
+                }
+                tempStack.push(tempElem);
+            }
+        }
+
+        while (! tempStack.empty()) {
+            stack.push(tempStack.pop());
+        }
+    }
+
+    private static void sortStackByStackOptition(Stack<Integer> stack) {
+        Stack<Integer> help = new Stack<>();
+        while (! stack.empty()) {
+            int element = stack.pop();
+            while (!help.empty() && help.peek() < element) {
+                stack.push(help.pop());
+            }
+            help.push(element);
+        }
+        while (! help.empty()) {
+            stack.push(help.pop());
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(3);
+        stack.push(10);
+        stack.push(1);
+        stack.push(5);
+        stack.push(2);
+
+        System.out.println(JSON.toJSONString(stack));
+//        sortStackByStack(stack);
+        sortStackByStackOptition(stack);
+        System.out.println(JSON.toJSONString(stack));
+    }
+}
---
+++ src/test/java/com/marvel/knowlege/UUIDTest.java
@@ -0,0 +1,10 @@
+package com.marvel.knowlege;
+
+import java.util.UUID;
+
+public class UUIDTest {
+    public static void main(String[] args) {
+        String result = UUID.randomUUID().toString();
+        System.out.println(result);
+    }
+}
