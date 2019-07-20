0d1c6ad4d0dfa36493cb47c7dcdb93a916c522ea
cug09evan <cug09evan@163.com>
2019-07-20 15:19:42 +0800

    DEV：JVM 测试

---
+++ src/main/java/com/god/jvm/HeapOOM.java
@@ -0,0 +1,21 @@
+package com.god.jvm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 测试java OOM 异常场景
+ */
+public class HeapOOM {
+    int i;
+    static class OOMObejct {
+
+    }
+
+    public static void main(String[] args) {
+        List<OOMObejct> list = new ArrayList<>();
+        while (true) {
+            list.add(new OOMObejct());
+        }
+    }
+}
